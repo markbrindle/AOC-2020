f62b4204a1282cd3290c4da77b89364fec2acd34
Mark Brindle <mark.brindle@arkemm.com>
2020-12-01 19:45:17 +0000

    Initial Commit

---
+++ AOC-2020/AOC_2020App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC_2020App.swift
+//  AOC-2020
+//
+//  Created by Mark Brindle on 01/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct AOC_2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AOC-2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AOC-2020
+//
+//  Created by Mark Brindle on 01/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
