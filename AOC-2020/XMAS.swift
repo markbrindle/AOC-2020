eae5099c53e583ed366012ba1a051a77170efe7f
Mark Brindle <mark.brindle@arkemm.com>
2020-12-11 15:27:44 +0000

    Day 9 Part 2

--- AOC-2020/XMAS.swift
+++ AOC-2020/XMAS.swift
@@ -31,4 +31,36 @@ struct XMAS {
         }
         return -1
     }
+    
+    static func exploit(for data: String, weakness: Int64) -> Int64 {
+        let cypher = data
+            .components(separatedBy: "\n")
+            .compactMap { Int64($0) }
+            
+        var sum = Int64(0)
+
+        for i in 0 ..< cypher.count {
+            sum = Int64(0)
+            var contiguousNumbers = [Int64]()
+            var n = i
+            while sum < weakness && n < cypher.count {
+                let num = cypher[n]
+                if sum + num > weakness {
+                    // This is not the exploit you are looking for
+                    break
+                }
+                contiguousNumbers.append(num)
+                
+                if sum + num == weakness && contiguousNumbers.count >= 1 {
+                    let sorted = contiguousNumbers.sorted()
+                    return sorted.first! + sorted.last!
+                }
+                sum += num
+                n += 1
+            }
+        }
+        
+        // Couldn't find an exploit
+        return -1
+    }
 }
--- AOC-2020Tests/XMASTests.swift
+++ AOC-2020Tests/XMASTests.swift
@@ -124,4 +124,86 @@ class XMASTests: XCTestCase {
         }
     }
 
+/*
+--- Part Two ---
+
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+
+Again consider the above example:
+
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+     
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+
+To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+
+What is the encryption weakness in your XMAS-encrypted list of numbers?
+
+*/
+    
+    func testEncrptionWeaknessForReturns62() throws {
+        // Given
+        let data =
+"""
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+"""
+        let expectedOutcome = Int64(62)
+        
+        // When
+        let outcome = XMAS.exploit(for: data, weakness:127)
+        
+        // Then
+        XCTAssertEqual(expectedOutcome, outcome, "Data should not be valid")
+    }
+    
+    func testEncrptionWeaknessFor138879426Returns23761694() throws {
+        // Given
+        let data = Day_09.input
+        let expectedOutcome = Int64(23761694)
+        
+        // When
+        let outcome = XMAS.exploit(for: data, weakness:138879426)
+        
+        // Then
+        XCTAssertEqual(expectedOutcome, outcome, "Data should not be valid")
+    }
+    
 }
